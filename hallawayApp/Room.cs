52e0f7f36c0fae9e6e382955d845c4a0d7b121da
Marchant01 <celvin.marchant@hotmail.com>
2024-11-25 13:42:50 +0100

    Modified Room class
    Added price, size and availability plus a constructor to the class.

--- hallawayApp/Room.cs
+++ hallawayApp/Room.cs
@@ -2,5 +2,14 @@ namespace hallawayApp;
 
 public class Room
 {
-    
+    private int _price;
+    private double _size;
+    private bool _isAvailable;
+
+    private Room(int price, double size, bool isAvailable)
+    {
+        this._price = price;
+        this._size = size;
+        this._isAvailable = isAvailable;
+    }
 }
