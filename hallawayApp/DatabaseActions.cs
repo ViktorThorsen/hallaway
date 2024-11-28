c3ca4f3e1f71550dc1b0420f8d64749c9097762f
Marchant01 <celvin.marchant@hotmail.com>
2024-11-28 09:50:59 +0100

    Added DatabaseActions
    Created a DatabaseActions with functions with missing bodies for
    creating and adding items to the their respective lists.

---
+++ hallawayApp/DatabaseActions.cs
@@ -0,0 +1,40 @@
+using Npgsql;
+
+namespace hallawayApp;
+
+public class DatabaseActions
+{
+    NpgsqlDataSource _db;
+
+    public DatabaseActions(NpgsqlDataSource db)
+    {
+        _db = db;
+    }
+    
+    public void ShowAllHotels()
+    {
+        await using (var cmd = _db.CreateCommand("SELECT * FROM Hotel"))
+        await using (var reader = await cmd.ExecuteReaderAsync())
+        {
+            while (await reader.ReadAsync())
+            {
+                // Hotel constructor and add to list
+            }
+        }
+    }
+
+    public void ShowAllPersons()
+    {
+        
+    }
+
+    public void GetOneHotel()
+    {
+        
+    }
+
+    public void GetOnePerson()
+    {
+        
+    }
+}
