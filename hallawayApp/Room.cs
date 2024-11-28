b81c822f219a8a18c1c9e78aed0c39039f890186
Marchant01 <celvin.marchant@hotmail.com>
2024-11-28 12:58:32 +0100

    Add GetHotel Method and necessary methods
    Created and added get hotel method which reads hotels from the database
    and returns them as objects. Also added necessary methods to make room/
    addon list and address objects.

--- hallawayApp/Room.cs
+++ hallawayApp/Room.cs
@@ -6,7 +6,7 @@ public class Room
     private double _size;
     private bool _isAvailable;
  
-    private Room(int price, double size, bool isAvailable)
+    public Room(int price, double size, bool isAvailable)
     {
         this._price = price;
         this._size = size;
